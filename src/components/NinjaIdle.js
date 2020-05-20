161fbfde87207b3b2b0f584fe530577ae204ac8c
KamajiiSC <brandonlschaen@gmail.com>
2020-05-20 19:36:19 -0400

    Turned NinjaIdle into a class that extends React.Component

--- src/components/NinjaIdle.js
+++ src/components/NinjaIdle.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+export default class NinjaIdle extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <StatBlock />
+        <TrainButton />
+      </div>
+    )
+  }
+};
+
 const TrainButton = () => (
   <div>
     <button>Train</button>
@@ -12,12 +24,3 @@ const StatBlock = () => (
     <h1>Training Points: </h1>
   </div>
 );
-
-const NinjaIdle = () => (
-  <div>
-    <StatBlock />
-    <TrainButton />
-  </div>
-);
-
-export default NinjaIdle;
